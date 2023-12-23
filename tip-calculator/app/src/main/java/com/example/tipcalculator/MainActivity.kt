14c8dd7913c46b27527b6e28bfac9a4e57371f4f
Ariel Armijo <arielarmijo@yahoo.es>
2023-12-23 10:25:58 -0300

    Add tip calculator tests

--- tip-calculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ tip-calculator/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -167,7 +168,8 @@ fun RoundTipRow(
  * according to the local currency.
  * Example would be "$10.00".
  */
-private fun calculateTip(
+@VisibleForTesting
+internal fun calculateTip(
   amount: Double,
   tipPercent: Double = 10.0,
   roundUp: Boolean,
