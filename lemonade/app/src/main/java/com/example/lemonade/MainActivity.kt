7ded015d5618621042f8c3296f079f6233cf42f3
Ariel Armijo <arielarmijo@yahoo.es>
2023-12-23 08:56:07 -0300

    Add lemonade app

--- lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
+++ lemonade/app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -35,14 +35,14 @@ import androidx.compose.ui.unit.dp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            LemonadeTheme {
-                LemonadeApp()
-            }
-        }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      LemonadeTheme {
+        LemonadeApp()
+      }
     }
+  }
 }
 
 
@@ -50,94 +50,94 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun LemonadeApp() {
-    var state by remember { mutableStateOf("tree") }
-    var taps by remember { mutableIntStateOf(0) }
+  var state by remember { mutableStateOf("tree") }
+  var taps by remember { mutableIntStateOf(0) }
 
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        text = "Lemonade",
-                        fontWeight = FontWeight.Bold
-                    )
-                },
-                colors = TopAppBarDefaults.smallTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-            )
-        }
-    ) { innerPadding ->
-
-        Surface(
-            color = MaterialTheme.colorScheme.background,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .background(MaterialTheme.colorScheme.tertiaryContainer),
-        ) {
-            when (state) {
-                "tree" -> {
-                    LemonadeState(
-                        imageId = R.drawable.lemon_tree,
-                        textId = R.string.tree,
-                        onClick = {
-                            state = "squeeze"
-                            taps = (1..4).random()
-                        }
-                    )
-                }
-
-                "squeeze" -> {
-                    LemonadeState(
-                        imageId = R.drawable.lemon_squeeze,
-                        textId = R.string.squeeze,
-                        onClick = {
-                            taps--
-                            if (taps == 0) {
-                                state = "drink"
-                            }
-                        }
-                    )
-                }
-
-                "drink" -> {
-                    LemonadeState(
-                        imageId = R.drawable.lemon_drink,
-                        textId = R.string.drink,
-                        onClick = { state = "restart" }
-                    )
-                }
-
-                "restart" -> {
-                    LemonadeState(
-                        imageId = R.drawable.lemon_restart,
-                        textId = R.string.restart,
-                        onClick = { state = "tree" }
-                    )
-                }
-            }
-        }
+  Scaffold(
+    topBar = {
+      CenterAlignedTopAppBar(
+        title = {
+          Text(
+            text = "Lemonade",
+            fontWeight = FontWeight.Bold
+          )
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+          containerColor = MaterialTheme.colorScheme.primaryContainer,
+          titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+          navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+          actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+      )
     }
+  ) { innerPadding ->
+
+    Surface(
+      color = MaterialTheme.colorScheme.background,
+      modifier = Modifier
+        .fillMaxSize()
+        .padding(innerPadding)
+        .background(MaterialTheme.colorScheme.tertiaryContainer),
+    ) {
+      when (state) {
+        "tree" -> {
+          LemonadeState(
+            imageId = R.drawable.lemon_tree,
+            textId = R.string.tree,
+            onClick = {
+              state = "squeeze"
+              taps = (1..4).random()
+            }
+          )
+        }
+
+        "squeeze" -> {
+          LemonadeState(
+            imageId = R.drawable.lemon_squeeze,
+            textId = R.string.squeeze,
+            onClick = {
+              taps--
+              if (taps == 0) {
+                state = "drink"
+              }
+            }
+          )
+        }
+
+        "drink" -> {
+          LemonadeState(
+            imageId = R.drawable.lemon_drink,
+            textId = R.string.drink,
+            onClick = { state = "restart" }
+          )
+        }
+
+        "restart" -> {
+          LemonadeState(
+            imageId = R.drawable.lemon_restart,
+            textId = R.string.restart,
+            onClick = { state = "tree" }
+          )
+        }
+      }
+    }
+  }
 }
 
 @Composable
 fun LemonadeState(imageId: Int, textId: Int, onClick: () -> Unit) {
-        Column (
-            Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Button(onClick = onClick) {
-                Image(painter = painterResource(id = imageId), contentDescription = "" )
-            }
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(
-                text = stringResource(textId),
-                color = MaterialTheme.colorScheme.primary
-            )
-        }
+  Column (
+    Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.Center,
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Button(onClick = onClick) {
+      Image(painter = painterResource(id = imageId), contentDescription = "" )
+    }
+    Spacer(modifier = Modifier.height(16.dp))
+    Text(
+      text = stringResource(textId),
+      color = MaterialTheme.colorScheme.primary
+    )
+  }
 }
